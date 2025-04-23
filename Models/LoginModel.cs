62da9e4b300f9053050219b5ac001488dd68488e
Owen <owenps123@gmail.com>
2025-04-22 20:35:39 -0500

    Add JWT authentication and new data models
    Updated `TorneosITM_API.csproj` to include new model files and dependencies for `DBExamenModel`. Added JWT settings in `Web.config` for authentication. Introduced `TokenGenerator.cs` for JWT token generation. Created new model classes: `AdministradorITM`, `LoginModel`, `LoginResponse`, and `Torneo`. Updated Entity Framework model files to reflect the database schema and relationships. Improved project organization with a new folder structure for models.

---
+++ Models/LoginModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TorneosITM_API.Models
+{
+    public class LoginModel
+    {
+        public string Usuario { get; set; }
+        public string Clave { get; set; }
+
+    }
+}
---
+++ Models/LoginResponse.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TorneosITM_API.Models
+{
+    public class LoginResponse
+    {
+        public string Usuario { get; set; }
+        public bool Autenticado { get; set; }
+        public string Token { get; set; }
+        public string Mensaje { get; set; }
+
+        // Constructor opcional para inicializar valores por defecto
+        public LoginResponse()
+        {
+            Usuario = string.Empty;
+            Autenticado = false;
+            Token = string.Empty;
+            Mensaje = string.Empty;
+        }
+    }
+}
